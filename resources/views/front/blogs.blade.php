bb93e8b75c6d4af8c5e63ec009540228e0aebe03
Aliafghari <Aliafghari4@gmail.com>
2023-08-02 00:04:24 +0330

    sort fronts

---
+++ resources/views/front/blogs.blade.php
@@ -0,0 +1,61 @@
+<section class="blogs py-5">
+    <div class="blogs-title text-center py-5">
+        <h2 class="fw-bold">بلاگ های من</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Deserunt, perferendis.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-lg-4">
+                <div class="blog-item shadow">
+                    <img src="{{ asset('front\picture\blog1.jpg') }}" class="w-100" alt="" />
+                    <div class="blog-text p-2">
+                        <h4 class="p-2 fw-bold fs-5 border-bottom">
+                            بلاگ 1
+                        </h4>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Obcaecati, alias?
+                        </p>
+                        <a href="" class="btn btn-danger w-100">بخوانید</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="blog-item shadow">
+                    <img src="{{ asset('front\picture\blog1.jpg') }}" class="w-100" alt="" />
+                    <div class="blog-text p-2">
+                        <h4 class="p-2 fw-bold fs-5 border-bottom">
+                            بلاگ 2
+                        </h4>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Obcaecati, alias?
+                        </p>
+                        <a href="" class="btn btn-danger w-100">بخوانید</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="blog-item shadow">
+                    <img src="{{ asset('front\picture\blog1.jpg') }}" class="w-100" alt="" />
+                    <div class="blog-text p-2">
+                        <h4 class="p-2 fw-bold fs-5 border-bottom">
+                            بلاگ 3
+                        </h4>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Obcaecati, alias?
+                        </p>
+                        <a href="" class="btn btn-danger w-100">بخوانید</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/front/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark py-5 mt-5">
+    <p class="text-white text-center m-0">طراحی توسط علی افقری</p>
+</footer>
