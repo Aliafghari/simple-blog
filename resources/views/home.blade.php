0c14bd6614ba043c5b22967b148eddf93daa0669
Aliafghari <Aliafghari4@gmail.com>
2023-08-03 02:08:52 +0330

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">تنظیمات اصلی سایت</div>
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        <ul>
+                            <li><a href="">تنظیمات بخش خانه</a></li>
+                            <li><a href="">تنظیمات بخش مهارت ها</a></li>
+                            <li><a href="">تنظیمات بخش بلاگ ها</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">داشبورد</div>
+
+                    <div class="card-body">
+
+                        شما میتوانید تنظیمات سایت را از سمت راست تغییر دهید.
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
