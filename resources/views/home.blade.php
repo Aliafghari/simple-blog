ce699f868b58ae1c7884335c643e82cbf2b0b1e8
Aliafghari <Aliafghari4@gmail.com>
2023-08-26 18:03:15 +0330

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">تنظیمات اصلی سایت</div>
+
+                    <div class="card-body">
+                        <ul>
+                            <li><a href="">تنظیمات بخش خانه</a></li>
+                            <li><a href="">تنظیمات بخش مهارت ها</a></li>
+                            <li><a href="">تنظیمات بخش بلاگ ها</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">داشبورد</div>
 
                 <div class="card-body">
                     @if (session('status'))
