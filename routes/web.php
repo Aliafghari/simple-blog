099fd9d2c69e6694189a6e260c09439a95dcc0fb
Aliafghari <Aliafghari4@gmail.com>
2023-08-26 18:03:09 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\administrator\homeController;
+use App\Http\Controllers\front\frontController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,15 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('/', function () {
-    return view('front.index');
-});
+// Route::get('/', function () {
+//     return view('front.index');
+// })->name('front');
+Route::get('/', [frontController::class,'index']);
 
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/home/home.page', function () {
+//     return view('admin.home.index');
+// })->name('home.page');
+Route::resource('/home/home', homeController::class)->parameters(['home'=>'id']);
