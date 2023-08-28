880ff9d24f4683e2d1fc4ea927d0bfdf3111ae1e
Aliafghari <Aliafghari4@gmail.com>
2023-08-28 21:52:23 +0330

    Update frontController.php

--- app/Http/Controllers/front/frontController.php
+++ app/Http/Controllers/front/frontController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use App\Models\Home;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class frontController extends Controller
@@ -11,6 +13,12 @@ class frontController extends Controller
     public function index()
     {
         $home = Home::orderBy('id', 'desc')->first();
-        return view('front.index', compact('home'));
+
+        $about=About::orderBy('id', 'desc')->first();
+
+        $skill=Skill::all()->take(6);
+        /*or:    $skill=Skill::orderBy('id', 'desc')->take(6)->get();  */
+
+        return view('front.index', compact('home','about','skill'));
     }
 }
