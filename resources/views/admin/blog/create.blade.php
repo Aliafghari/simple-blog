ba225a640cb7fb78832b0c014a5a41e51a8c2830
Aliafghari <Aliafghari4@gmail.com>
2023-09-08 20:05:47 +0330

    Create create.blade.php

---
+++ resources/views/admin/blog/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header">بازگشت</div>
+
+                    <div class="card-body">
+                        <ul>
+                            <li><a href="{{ route('blog.index') }}">تنظیمات بلاگ ها</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                @foreach (['title', 'description','image'] as $field)
+                    @error($field)
+                        <div class="alert alert-danger mt-3">{{ $message }}</div>
+                    @enderror
+                @endforeach
+
+            </div>
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">افزودن مقادیر جدید </div>
+
+                    <div class="card-body">
+                        {{-- برای ارسال عکس در فرم :
+                        enctype="multipart/form-data" --}}
+                        <form action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group mt-3">
+                                <label for="">موضوع</label>
+                                <input type="text" class="form-control" name="title">
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">متن</label>
+                                <textarea type="text" class="form-control" name="description"></textarea>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">عکس</label>
+                                <input type="file" class="form-control" name="image">
+                            </div>
+                            <div class="form-group mt-3">
+                                <button type="submit" class="btn btn-success px-4">ذخیره</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
